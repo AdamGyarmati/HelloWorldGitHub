cffcc004ad1edbbc7be692eca7e65bdd992174de
AdamGyarmati <gyari12@gmail.com>
2023-07-20 14:30:09 +0200

    Revert "Added a new revert line"
    This reverts commit 5c43e41ea865d17be3b16aaf4d4551d35379db00.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello again from github!!!");
         System.out.println("Did some changes!");
-        System.out.println("We will revert this line!");
     }
 }
